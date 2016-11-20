7841b554c146fc8553e7d5fc3e5163c222efbea0
Oleksandr Mandryk <icoderman@gmail.com>
2016-11-20 14:21:08 +0100

    Retrieving Data Using HTTP

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -14,6 +14,8 @@ export class ProductListComponent implements OnInit {
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string;
+    errorMessage: string;
+
     products: IProduct[];
 
     constructor(private _productService: ProductService) {
@@ -25,7 +27,9 @@ export class ProductListComponent implements OnInit {
 
     ngOnInit(): void {
         console.log('In OnInit');
-        this.products = this._productService.getProducts();
+        this._productService.getProducts()
+        .subscribe(products => this.products = products,
+        error => this.errorMessage = <any>error);
     }
 
     onRatingClicked(message: string): void {
