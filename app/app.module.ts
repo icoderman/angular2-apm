fa82c25271f2e7ba18cdaf8a76a122b78b7e438c
Oleksandr Mandryk <icoderman@gmail.com>
2016-11-20 10:05:14 +0100

    Building Nested Components

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,10 +4,15 @@ import {FormsModule} from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import { ProductListComponent }  from './products/product-list.component';
+import { ProductFilterPipe} from './products/product-filter.pipe';
+import { StarComponent } from './shared/star.component'
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, ProductListComponent ],
+  declarations: [ AppComponent, 
+  ProductListComponent, 
+  ProductFilterPipe,
+  StarComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
